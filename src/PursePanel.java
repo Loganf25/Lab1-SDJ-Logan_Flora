b93c57947e610218711a7a91a17593946a73706c
loganf25 <lflora@cub.uca.edu>
2024-09-04 10:31:48 -0500

    More Added comments to describe each file for readability

--- src/PursePanel.java
+++ src/PursePanel.java
@@ -1,4 +1,4 @@
-//Purse Panel Class- Holds gui portion of the purse, which displays images of denominations instead of words
+//Purse Panel Class - Holds gui portion of the purse, which displays images of denominations instead of words
 import javax.swing.*;
 import java.awt.*;
 
@@ -10,9 +10,12 @@ public class PursePanel extends JPanel {
     }
 
 
+    //I could not figure out how to add the purse to this class, so this is how I did it
     void setPurse(Purse purse) {
         this.purse = purse;
     }
+
+    //Code for displaying the purse in picture format instead of words in the register class
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         if (purse != null) {
@@ -26,20 +29,24 @@ public class PursePanel extends JPanel {
                 while(left >= curr.amt()) {
                     left -= curr.amt();
                     count++;
+                    //Just a different way to make up for the "fake math," especially after a cent left
                     left = Math.round(left * 100.00) / 100.00;
                 }
 
                 // Draw the images for the denomination
+                //Found this way to resize the images
                 for (int i = 0; i < count; i++) {
-                    // Load the image from the file path and print
+                    // Load the image from the file path and print whilst also sizing it
+                    //Does lower image quality for me though
                     Image image = new ImageIcon(curr.img()).getImage();
                     g.drawImage(image, x, y, 100, 50, this);
 
-                        // Update x-coordinate for the next image
+                        // Update x-coordinate for the next image so that they are displayed next to each other
+                        //Rather than on top of another
                         x += 110;
-                        if (x > getWidth() - 60) { // Check if the x-coordinate exceeds the panel width
-                            x = 10; // Reset x to start a new row
-                            y += 55; // Move to the next row
+                        if (x > getWidth() - 60) {
+                            x = 10;
+                            y += 55;
                         }
                 }
             }
